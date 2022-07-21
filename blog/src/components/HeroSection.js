c683432d70be30e665c3655f6362eefe7a67b77f
hayesjordan1 <jordan.hayes@cdpipelinedevshop.com>
2022-07-21 16:52:29 -0400

    second commit

--- blog/src/components/HeroSection.js
+++ blog/src/components/HeroSection.js
@@ -2,16 +2,25 @@ import React from 'react';
 import '../App.css'
 import {Button} from './Button';
 import './HeroSection.css';
+import video from'./Videos/testvideo.mp4';
 
+console.log(video);
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src="/Videos/testvideo.mp4" autoPlay loop muted />
-        <h1>PLACE HOLDER</h1>
-        <p>This is a sample text</p>
-        <div className="hero-btns"></div>
-        <Button className='btns' buttonStyle='btn--outline'
-        buttonSize='btn--large'>PLACE HOLDER</Button>
+        <video src={video} autoPlay loop muted />
+        <h1>THEATER BLOG</h1>
+        <p>Acting is an art form</p>
+        <div className="hero-btns">
+        <Button className='btns' 
+        buttonStyle='btn--outline'
+        buttonSize='btn--large'
+        >GET STARTED</Button>
+        <Button className='btns' 
+        buttonStyle='btn--primary'
+        buttonSize='btn--large'
+        >PLACE HOLDER <i className='far fa-play-circle' /></Button>
+        </div>
     </div>
   )
 }
